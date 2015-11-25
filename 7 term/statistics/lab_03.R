8fa8b8f4f765f98060d11fafe02bec1c6e497613
Pastafarianist <mr.pastafarianist@gmail.com>
2015-11-25 03:35:47 +0300

    Finished Lab 3

--- 7 term/statistics/lab_03.R
+++ 7 term/statistics/lab_03.R
@@ -7,7 +7,6 @@ addBias <- function(X) {
 ordinaryLeastSquares <- function (X, Y) {
   tX <- t(X)
   res_col <- solve(tX %*% X, tX %*% Y)
-  # return(t(res_col))
   return(res_col)
 }
 
@@ -24,6 +23,44 @@ residualVariance <- function (X, Y, Theta) {
   return(rss / (n - m))
 }
 
+confidenceIntervalCoefficients <- function (X, rv, Theta, alpha) {
+  n <- nrow(X)
+  m <- ncol(X)
+  tt <- qt(1 - alpha / 2, n - m)
+  B <- t(X) %*% X
+  h <- tt * rv * sqrt(diag(solve(B)))
+  left <- Theta - h
+  right <- Theta + h
+  return(cbind(left, right))
+}
+
+confidenceIntervalVariance <- function (rss, X, alpha) {
+  n <- nrow(X)
+  m <- ncol(X)
+  left <- rss / qchisf(1 - alpha / 2, n - m)
+  right <- rss / qchisf(alpha / 2, n - m)
+  return(cbind(left, right))
+}
+
+confidenceIntervalPrognosis <- function (personal_X, Theta, rv, X, alpha) {
+  n <- nrow(X)
+  m <- ncol(X)
+  tt <- qt(1 - alpha / 2, n - m)
+  h1 <- personal_X %*% Theta
+  h2 <- tt * rv * sqrt(personal_X %*% solve(t(X) %*% X) %*% t(personal_X))
+  left <- h1 - h2
+  right <- h1 + h2
+  return(cbind(left, right))
+}
+
+checkCoefficientsSignificance <- function (theta_intervals) {
+  res <- rep(TRUE, nrow(theta_intervals))
+  for (i in 1:nrow(theta_intervals)) {
+    res[i] <- ((theta_intervals[i, 1] <= 0) && (theta_intervals[i, 2] >= 0))
+  }
+  return(res)
+}
+
 # Sets current directory to the path of script file.
 # Fragile, works only when the script is loaded via source().
 setwd(dirname(sys.frame(1)$ofile))
@@ -34,16 +71,34 @@ X <- addBias(X)
 Y <- data.matrix(data[1])
 
 Theta <- ordinaryLeastSquares(X, Y)
+rss <- residualSumOfSquares(X, Y, Theta)
 rv <- residualVariance(X, Y, Theta)
 fit <- lm(height ~ ., data=data)
 
+print("Linear regression coefficients according to me:")
 print(Theta)
+print("Linear regression coefficients according to the standard library:")
 print(fit$coefficients)
+
+print("Estimation of the residual variance:")
 print(rv)
 
 personal_data = read.csv('personal_data.csv')
-print(personal_data[1])
+personal_X = data.matrix(personal_data[2:length(data)])
+personal_X = addBias(personal_X)
 
-# summary(fit)
-# fitted(fit)
-# residuals(fit)
+print("Predicting personal data. My actual height:")
+print(personal_data[1])
+print("My predicted height:")
+print(personal_X %*% Theta)
+
+print("Confidence intervals for coefficients:")
+theta_intervals <- confidenceIntervalCoefficients(X, rv, Theta, 0.95)
+print(theta_intervals)
+print("Confidence interval for the variance:")
+print(confidenceIntervalVariance(rss, X, 0.95))
+print("Confidence interval for the personal prognosis:")
+print(confidenceIntervalPrognosis(personal_X, Theta, rv, X, 0.95))
+
+print("The significance of the coefficients:")
+print(checkCoefficientsSignificance(theta_intervals))
